d695a07af273c1c0a0c5a920428e71c9e3b79cd2
LucianoEsplai <lfrancoepifani@gmail.com>
2023-10-29 10:48:29 +0100

    Starting NavBar

---
+++ FunDit/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Icon } from '@iconify/react';
+
+
+export default function NavBar() {
+  return (
+    <div>
+        <nav className='navbar'>
+            <Icon icon="material-symbols:menu" />
+           
+        </nav>
+    </div>
+  )
+}
